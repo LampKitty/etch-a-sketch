cbf9737acef95ab5ea50811119977a0e54b4b1e6
LampKitty <ranpukatu@gmail.com>
2022-08-26 14:06:03 +0300

    Make a square-aligned flexbox grid

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 const container = document.querySelector('.container');
 
+// loop for creating 16 divs
 for(let i = 1; i <= 16; i++) {
     const div = document.createElement(`div`);
     div.classList.add(`div${i}`);
     container.appendChild(div);
 }
+
+const divList = document.querySelectorAll('.container>div');
+// iterate elements of container and apply attributes to them
+divList.forEach(element => 
+    element.setAttribute('style', 
+    'min-height: 25%; \
+    min-width: 25%; \
+    background:#5b6967; \
+    ')
+)
 
