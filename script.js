23609010e9fe00aab1016696045ea25b94adc606
LampKitty <ranpukatu@gmail.com>
2022-08-26 15:01:10 +0300

    add a button prompting user input

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.container');
 // loop for creating 16 divs
-for(let i = 1; i <= 16; i++) {
+for (let i = 1; i <= 16; i++) {
     const div = document.createElement(`div`);
     div.classList.add(`div${i}`);
     container.appendChild(div);
@@ -8,19 +8,22 @@ for(let i = 1; i <= 16; i++) {
 
 const divList = document.querySelectorAll('.container>div');
 // iterate elements of container and apply attributes to them
-divList.forEach(element => 
-    element.setAttribute('style', 
+divList.forEach(element =>
+    element.setAttribute('style',
     'min-height: 25%; \
     min-width: 25%; \
-    background:#5b6967; \
-    ')
+    background:#a0bdbb; ')
 )
 
 // add 'mouseover' event listeners
 divList.forEach(element =>
-        element.addEventListener('mouseover', () => 
-        element.style.background = '#fc705d')
-    );
+    element.addEventListener('mouseover', () =>
+        element.style.background = '#f7c3c3')
+);
 
-
+const tilesButton = document.querySelector('.tiles');
+function askUserInput() {
+    tilesButton.addEventListener('click', () => 
+    prompt('How many tiles per side?'))
+}
 
